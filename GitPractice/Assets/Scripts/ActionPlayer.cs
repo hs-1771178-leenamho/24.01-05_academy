cad8f7eccede117b7156a8bf704ecbd5aadf70df
KongJin <160682840+Borntentacle@users.noreply.github.com>
2024-02-27 12:20:38 +0900

    스크립트 추가
    ActionPlayer 추가
    RoontMotion 추가

---
+++ GitPractice/Assets/Scripts/ActionPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ActionPlayer : CharacterProperty
+{
+    int hashJumpAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hashJumpAttack = Animator.StringToHash("Jumpattack");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float x = Input.GetAxis("Horizontal");
+       float y = Input.GetAxis("Vertical");
+        myAnim.SetFloat("X", x);
+        myAnim.SetFloat("Y", y);
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            myAnim.SetTrigger(hashJumpAttack);
+        }
+    }
+}
---
+++ GitPractice/Assets/Scripts/RootMotion.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootMotion : MonoBehaviour
+{
+    Animator myAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnAnimatorMove()
+    {
+        transform.parent.position += myAnim.deltaPosition;
+        transform.parent.rotation *= myAnim.deltaRotation;
+    }
+
+}
